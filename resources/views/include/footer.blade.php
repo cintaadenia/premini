ca77f3cd8ea4cbe75dd55303f514632b76954692
cintaadenia <cintaadenia238@gmail.com>
2024-04-02 08:46:11 +0700

    hai

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,12 @@
+{{-- footer --}}
+
+
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2024 &copy; CS</p>
+        </div>
+        <div class="float-end"></span> by <a href="https://saugi.me">Cinta & Syahfa</a></p>
+        </div>
+    </div>
+</footer>
