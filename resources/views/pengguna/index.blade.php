4022197ea3a038314e0de6dd1b48619c199e1dcc
anandasyahfa <165118827+anandasyahfa@users.noreply.github.com>
2024-04-02 16:06:22 +0700

    perubahan tampilan

--- resources/views/pengguna/index.blade.php
+++ resources/views/pengguna/index.blade.php
@@ -754,18 +754,18 @@ https://mo.com/tm-586-scholar
                             <div class="col-lg-3">
                                 <div class="image">
                                     <img src="
-                  user/assets/images/event-01.jpg" alt="">
+                  user/assets/images/gacoan.jpg" alt="">
                                 </div>
                             </div>
                             <div class="col-lg-9">
                                 <ul>
                                     <li>
-                                        <span class="category">Web Design</span>
-                                        <h4>UI Best Practices</h4>
+                                        <span class="category">Cabang Gacoan</span>
+                                        <h4>Singosari Malang</h4>
                                     </li>
                                     <li>
                                         <span>Date:</span>
-                                        <h6>16 Feb 2036</h6>
+                                        <h6>16 Feb 2019</h6>
                                     </li>
                                     <li>
                                         <span>Duration:</span>
@@ -773,7 +773,7 @@ https://mo.com/tm-586-scholar
                                     </li>
                                     <li>
                                         <span>Price:</span>
-                                        <h6>$120</h6>
+                                        <h6>10rb</h6>
                                     </li>
                                 </ul>
                                 <a href="#"><i class="fa fa-angle-right"></i></a>
@@ -787,18 +787,18 @@ https://mo.com/tm-586-scholar
                             <div class="col-lg-3">
                                 <div class="image">
                                     <img src="
-                  user/assets/images/event-02.jpg" alt="">
+                  user/assets/images/gacoan2.jpeg" alt="">
                                 </div>
                             </div>
                             <div class="col-lg-9">
                                 <ul>
                                     <li>
-                                        <span class="category">Front End</span>
-                                        <h4>New Design Trend</h4>
+                                        <span class="category">Cabang Gacoan</span>
+                                        <h4>Kota Batu</h4>
                                     </li>
                                     <li>
                                         <span>Date:</span>
-                                        <h6>24 Feb 2036</h6>
+                                        <h6>24 Feb 2020</h6>
                                     </li>
                                     <li>
                                         <span>Duration:</span>
@@ -820,18 +820,18 @@ https://mo.com/tm-586-scholar
                             <div class="col-lg-3">
                                 <div class="image">
                                     <img src="
-                  user/assets/images/event-03.jpg" alt="">
+                  user/assets/images/gacoan3.jpg" alt="">
                                 </div>
                             </div>
                             <div class="col-lg-9">
                                 <ul>
                                     <li>
-                                        <span class="category">Full Stack</span>
-                                        <h4>Web Programming</h4>
+                                        <span class="category">Cabang Surabaya</span>
+                                        <h4>Jalan Dupak Surabaya</h4>
                                     </li>
                                     <li>
                                         <span>Date:</span>
-                                        <h6>12 Mar 2036</h6>
+                                        <h6>12 Mar 2023</h6>
                                     </li>
                                     <li>
                                         <span>Duration:</span>
