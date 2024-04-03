a5cbd2b2d63fc97a642a07035286ad85c187c0a6
cintaadenia <cintaadenia238@gmail.com>
2024-04-03 09:43:42 +0700

    revisi

---
+++ resources/views/revisi.blade.php
@@ -0,0 +1,7 @@
+1.Login harus ada di navbar dan setelah login kemabli ke halaman user dan login berubah jadi logout, saat user pesan harus login
+
+2.form pesan tidak perlu nama
+
+3.ada inputan bukti pembayaran, dan di admin di ACC
+
+4.tombol slide
