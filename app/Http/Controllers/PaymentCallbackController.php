6cb2ac63701f3998affb53d361e04e2a30fb816c
anandasyahfa <165118827+anandasyahfa@users.noreply.github.com>
2024-04-29 11:56:50 +0700

    pay2

--- app/Http/Controllers/PaymentCallbackController.php
+++ app/Http/Controllers/PaymentCallbackController.php
@@ -19,6 +19,14 @@ class PaymentCallbackController extends Controller
 
             if ($request->transaction_status === 'settlement') {
                     $trans->statusBayar = 'PAID';
+                    // Logika pengurangan stok
+                    // $order = Order::find($trans->order_id);
+                    // $order->stock -= $trans->quantity;
+                    // dd($order);
+                    // $order->save();
+
+                    // Ubah status pembayaran
+                    // $trans->statusBayar = 'PAID';
 
                 $trans->save();
 
